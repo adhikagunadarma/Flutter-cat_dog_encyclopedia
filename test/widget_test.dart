7e6c534c3f484cd2e0a1e8d100f00906244561a9
unknown <adhikagunadarma@gmail.com>
2018-10-29 18:18:18 +0700

    Add Pics

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cat_dog_encyclopedia/ui/main.dart';
+import 'package:cat_dog_encyclopedia/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
